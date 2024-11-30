33de2b00ddcf7bbb97b814197954047c46dbd454
‍김용빈 <yongbin97@yonsei.ac.kr>
2024-11-30 19:57:53 +0900

    init project

---
+++ src/test/java/com/myclass360/myclass360/MyClass360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myclass360.myclass360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClass360ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
